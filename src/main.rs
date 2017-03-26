195f300f8ed816864a96db9e9c85d1bbde114a58
Dario Seyb <dario.seyb@gmail.com>
2017-03-26 12:43:49 +0200

    added serde for json stuff

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ extern crate iron;
 extern crate params;
 extern crate mongodb;
 
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
 use iron::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct BotRegistrationInfo {
+    name: String,
+    author: String
+}
 
 fn handle_user(req: &mut Request) -> IronResult<Response> {
     use params::{Params, Value};
@@ -21,5 +33,15 @@ fn handle_user(req: &mut Request) -> IronResult<Response> {
 }
 
 fn main() {
+    
+    let reg_info = BotRegistrationInfo {
+        name: "Test Bot".to_owned(),
+        author: "Dario".to_owned()
+    };
+
+    let j = serde_json::to_string(&reg_info).unwrap();
+
+    println!("Test {}", j);
+
     Iron::new(handle_user).http("127.0.0.1:3000").unwrap();
 }
